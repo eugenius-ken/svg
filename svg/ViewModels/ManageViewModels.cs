8b6306254601bc26a2ac0e42660c868e31c26a8c
Eugene <Eugene_ken@hotmail.com>
2016-10-12 15:00:45 +0500

    Updated some packages and replaced some classes

--- svg/ViewModels/ManageViewModels.cs
+++ svg/ViewModels/ManageViewModels.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
 
-namespace svg.Models
+namespace svg.ViewModels
 {
     public class IndexViewModel
     {
